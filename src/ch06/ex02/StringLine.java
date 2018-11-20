061841e247493e62b51287379b6d1a309d500843
Larson <larsona@outlook.com>
2018-11-20 12:42:53 -0600

    6.2

---
+++ src/ch06/ex02/StringLine.java
@@ -0,0 +1,23 @@
+package ch06.ex02;
+        
+import java.util.Scanner;
+        
+public class StringLine
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Input a line of text.");
+        System.out.print("> ");
+        String thing = input.nextLine();
+
+        Scanner thingScan = new Scanner(thing);
+        thingScan.useDelimiter("");
+        while (thingScan.hasNext()) {
+            System.out.println(thingScan.next());
+        }
+        
+        input.close();
+    }
+}
