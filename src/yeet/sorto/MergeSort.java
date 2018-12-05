26c30ea0b07e30646b164d79452a2c8f921a9e63
Larson <larsona@outlook.com>
2018-12-05 09:40:25 -0600

    WORKING MERGESORT KALKDSLKDSALKDSA

--- src/yeet/sorto/MergeSort.java
+++ src/yeet/sorto/MergeSort.java
@@ -19,28 +19,46 @@ public class MergeSort
         input.add(39);
         input.add(100);
         input.add(227);
+        input.add(500);
         System.out.println(sort(input));
     }
     private static ArrayList<Integer> sort(ArrayList<Integer> list) {
             ArrayList<Integer> list2 = list;
         if (list2.size() == 1)
             return list2;
-            if(list2.size() == 2) {
-                if(list2.get(0) > list2.get(1)) {
-                    int t = list2.get(0);
-                    list2.set(0, list2.get(1));
-                    list2.set(1, t);
-                }
-                return list2;
+        if(list2.size() == 2) {
+            if(list2.get(0) > list2.get(1)) {
+                int t = list2.get(0);
+                list2.set(0, list2.get(1));
+                list2.set(1, t);
             }
+            return list2;
+        }
         else {
             ArrayList<Integer> t1, t2;
-                t1 = new ArrayList<Integer>(list2.subList(0, (int)Math.round(Math.ceil(list2.size()/2f) - 1)));
+                t1 = new ArrayList<Integer>(list2.subList(0, (int)Math.round(Math.ceil(list2.size()/2f))));
                 t2 = new ArrayList<Integer>(list2.subList((int)Math.round(Math.ceil(list2.size()/2d)), list2.size()));
                 sort(t1);
                 sort(t2);
-                list2.addAll(t1);
-                list2.addAll(t2);
+                list2.clear();
+                while (t2.size() != 0 && t1.size() != 0) {
+                    if(t1.get(0) < t2.get(0)){
+                        list2.add(t1.get(0));
+                        t1.remove(0);
+                    }
+                    else{
+                        list2.add(t2.get(0));
+                        t2.remove(0);
+                    }
+                }
+                if(t1.size() == 0){
+                    list2.addAll(t2);
+                    t2.clear();
+                }
+                else{
+                    list2.addAll(t1);
+                    t1.clear();
+                }
                 return list2;
             }
         }
