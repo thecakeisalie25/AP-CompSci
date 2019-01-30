3c84e323b59d3c4d6aad9514a6eadab73881cadb
Larson <larsona@outlook.com>
2019-01-30 12:17:41 -0600

    done with 8

--- src/ch08/ex08/PostMaster.java
+++ src/ch08/ex08/PostMaster.java
@@ -1,16 +1,21 @@
 package ch08.ex08;
-        
+
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class PostMaster {
     public static void main(String[] args) throws FileNotFoundException
     {
         Scanner input = new Scanner(new File("src/ch08/ex08/zippy.txt"));
-        
-        
-        
+        ArrayList<Address> yeets = new ArrayList<Address>();
+        while (input.hasNext()) {
+            yeets.add(new Address(input.next(), input.next(), input.nextInt()));
+        }
+        for (Address e : yeets) {
+            System.out.println(e);
+        }
         input.close();
     }
 }
